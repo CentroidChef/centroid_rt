195f6784efb3bc7f85ed6bb0607970c9802545e9
Kirt Martin <kirt.martin@centroid.com>
2017-12-21 11:17:55 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# centroid_rt
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: centroid_rt
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
